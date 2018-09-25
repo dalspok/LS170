9da896b06b5f71bf9ad04e7cc5c27baee5767a16
Dalibor Spok <dalspok@gmail.com>
2018-09-25 07:06:30 +0200

    _

---
+++ users_and_interests/users_and_interests.rb
@@ -0,0 +1,31 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "pry"
+require "yaml"
+
+
+before do
+  @users = Psych.load_file("data/users.yaml")
+  @user_names = @users.keys
+end
+
+
+get "/" do
+  erb :home
+end
+
+get "/:name" do
+  user = params[:name].to_sym
+  redirect "/" unless @user_names.include? user
+  @user = @users[user]
+  erb :user
+end
+
+helpers do
+  def count_interests
+    @users.reduce(0) do |memo, (_, user)|
+      memo + user[:interests].size
+    end
+  end
+end
